c1e89d622e1873b2060a8eda33ce0201de19da4e
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-02 14:50:19 +0800

    AC (2ms, 112KB)

---
+++ C/a003.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int m, d, s;
+
+int main()
+{
+	while (scanf(" %d %d", &m, &d) == 2)
+	{
+		s = (m * 2 + d) % 3;
+		if (s == 0)
+		{
+			puts("普通");
+		}
+		else if (s == 1)
+		{
+			puts("吉");
+		}
+		else
+		{
+			puts("大吉");
+		}
+	}
+	return 0;
+}
