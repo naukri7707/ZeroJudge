ee7383bdc216b8807bac4af7eb3652e2b2c99e90
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-02 14:50:26 +0800

    AC (2ms, 84KB)

---
+++ C/a004.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int y;
+
+int main()
+{
+    while (scanf(" %d", &y) == 1)
+    {
+        if (y % 400 == 0)
+        {
+            puts("閏年");
+        }
+        else if (y % 4 == 0 && y % 100 != 0)
+        {
+            puts("閏年");
+        }
+        else
+        {
+            puts("平年");
+        }
+    }
+    return 0;
+}
