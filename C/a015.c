53c3fe3748d7f44ed8b701c10b5a7daeb784ea60
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-04 00:09:20 +0800

    AC (2ms, 92KB)

---
+++ C/a015.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int row = 0, col = 0 , matrix[100][100];
+
+int main()
+{
+    while (scanf(" %d %d", &row, &col) == 2)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+                scanf(" %d", &matrix[j][i]);
+        }
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+                printf("%d ", matrix[i][j]); // 反轉
+            putchar('\n');
+        }
+    }
+    return 0;
+}
