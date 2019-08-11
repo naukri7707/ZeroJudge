9902cffda27b2317c12d29cae082c4bc66e1fd0b
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 00:39:43 +0800

    AC (2ms, 76KB)

---
+++ C/a058.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int n , m;
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        int rem[3] = { 0 };
+        while (n--)
+        {
+            scanf(" %d", &m);
+            rem[m % 3]++;
+        }
+        printf("%d %d %d\n", rem[0], rem[1], rem[2]);
+    }
+    return 0;
+}
