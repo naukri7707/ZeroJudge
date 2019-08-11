20d94b9807f50e5892d4782b5d94dfb98b267879
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-11 10:55:04 +0800

    AC (2ms, 64KB)

---
+++ C/a034.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int n;
+char out[35], * ans;
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        ans = &out[34];
+        while (n)
+        {
+            *ans-- = (n & 1) + '0';
+            n >>= 1;
+        }
+        puts(++ans);
+    }
+    return 0;
+}
