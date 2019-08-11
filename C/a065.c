d480f51702f61215afae5238d3736b4a8e8c94a2
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 01:27:11 +0800

    AC (10ms, 84KB)

---
+++ C/a065.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+char str[10];
+
+int main()
+{
+    while (scanf(" %s", str) == 1)
+    {
+        for (int i = 0; i < 6; i++)
+            printf("%d", str[i] > str[i + 1] ? str[i] - str[i + 1] : str[i + 1] - str[i]);
+        putchar('\n');
+    }
+    return 0;
+}
