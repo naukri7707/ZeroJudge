d423fdb6d2dccf4d0ea382b507a38c723ad2ea0c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 01:09:53 +0800

    AC (2ms, 92KB)

---
+++ C/a059.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#define MAX 32
+
+int kase, f, t, sq[MAX];
+
+int main()
+{
+    for (int i = 0; i < MAX; i++)
+        sq[i] = i * i;
+    while (scanf(" %d", &kase) == 1)
+    {
+        for (int kas = 1; kas <= kase; kas++)
+        {
+            scanf(" %d %d", &f, &t);
+            int i = 0, sum = 0;
+            while (sq[i] < f)
+                i++;
+            do
+            {
+                sum += sq[i];
+            } while (++i < MAX && sq[i] <= t);
+            printf("Case %d: %d\n", kas, sum);
+        }
+    }
+    return 0;
+}
