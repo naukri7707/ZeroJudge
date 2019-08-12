b0dc01d95e6391ce6050f54edc3eadcc143a2356
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 20:16:35 +0800

    AC (4ms, 108KB)

---
+++ C/a147.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int n;
+
+int main()
+{
+    while (scanf(" %d", &n) == 1 && n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if (i % 7)
+                printf("%d ", i);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
