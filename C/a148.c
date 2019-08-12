db378940c1d03e24671d8344a76a278ca9264030
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 20:26:38 +0800

    AC (2ms, 80KB)

---
+++ C/a148.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int n, m, sum;
+    while (scanf(" %d", &n) == 1)
+    {
+        sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            scanf(" %d", &m);
+            sum += m;
+        }
+        puts(sum > 59 * n ? "no" : "yes");
+    }
+    return 0;
+}
