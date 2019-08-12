e1d46d4649257b4152293d7c827a2bfeb54e2657
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 20:57:07 +0800

    AC (2ms, 452KB)

---
+++ C/a216.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#define MAX 30001
+
+int n;
+int  f[MAX];
+long long g[MAX];
+
+int main()
+{
+    for (int i = 1; i < MAX; i++)
+    {
+        f[i] = i + f[i - 1];
+        g[i] = f[i] + g[i - 1];
+    }
+    while (scanf(" %d", &n) == 1)
+    {
+        printf("%d %lld\n", f[n], g[n]);
+    }
+    return 0;
+}
