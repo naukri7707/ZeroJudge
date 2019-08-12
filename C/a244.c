f05070e105086ee91b6505cbb73ff4634bf868de
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 00:15:19 +0800

    AC (6ms, 80KB)

---
+++ C/a244.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int n, a;
+long long b, c;
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        while (n--)
+        {
+            scanf(" %d %lld %lld", &a, &b, &c);
+            printf("%lld\n", a == 1 ? b + c : a == 2 ? b - c : a == 3 ? b * c : b / c);
+        }
+    }
+    return 0;
+}
