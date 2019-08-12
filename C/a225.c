15da60a26d75194d31a43e13b27eb5fd52fc4e63
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 21:45:27 +0800

    AC (4ms, 92KB)

---
+++ C/a225.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int n, list[1010];
+
+int cmp(const void* lhs, const void* rhs)
+{
+    int res = *(int*)lhs % 10 - *(int*)rhs % 10;
+    return res ? res : *(int*)rhs - *(int*)lhs;
+}
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        for (int i = 0; i < n; i++)
+            scanf(" %d", &list[i]);
+        qsort(list, n, sizeof(int), cmp);
+        for (int i = 0; i < n; i++)
+            printf("%d ", list[i]);
+        putchar('\n');
+    }
+    return 0;
+}
