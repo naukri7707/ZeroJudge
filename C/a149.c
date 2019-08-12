8bd4126c3d678ef19879bb5be78c1e9f4daef5d9
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 20:34:30 +0800

    AC (2ms, 80KB)

---
+++ C/a149.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int kase, sum;
+char n[15];
+
+int main()
+{
+    scanf(" %d", &kase);
+    while (kase--)
+    {
+        scanf(" %s", n);
+        sum = 1;
+        for (int i = 0; n[i]; i++)
+            sum *= n[i] - '0';
+        printf("%d\n", sum);
+    }
+    return 0;
+}
