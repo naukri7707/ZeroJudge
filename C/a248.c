1a68a8096648882616d7a382cef295a77826a44c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 00:41:55 +0800

    AC (49ms, 100KB)

---
+++ C/a248.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+int a, b, n;
+char output[10010], * oTop;
+
+int main()
+{
+    while (scanf(" %d %d %d", &a, &b, &n) == 3)
+    {
+        oTop = output;
+        printf("%d.", a / b);
+        while (n--)
+        {
+            a %= b;
+            a = (a << 3) + (a << 1);
+            *oTop++ = (a / b) + '0';
+        }
+        *oTop = '\0';
+        puts(output);
+    }
+    return 0;
+}
