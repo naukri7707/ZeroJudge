b73a2f960188e3565992b18b7101618c7b6a8874
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-12 20:48:52 +0800

    AC (2ms, 88KB)

---
+++ C/a215.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int n, m, sum, count;
+
+int main()
+{
+    while (scanf(" %d %d", &n, &m) == 2)
+    {
+        sum = n , count = 1;
+        while (sum <= m)
+            sum += n + count++;
+        printf("%d\n", count);
+    }
+    return 0;
+}
