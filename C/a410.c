9ad7fc0630149f749f0e6d31f78d334277385eb6
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 14:27:48 +0800

    AC (2ms, 120KB)

---
+++ C/a410.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+double a, b, c, d, e, f, x, y;
+
+int main()
+{
+    scanf(" %lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &e, &f);
+    if (a * e == d * b)
+        puts(b * f == e * c ? "Too many" : "No answer");
+    else
+        printf("x=%.2lf\ny=%.2lf\n", (c * e - b * f) / (a * e - d * b), (d * c - a * f) / (b * d - a * e));
+    return 0;
+}
