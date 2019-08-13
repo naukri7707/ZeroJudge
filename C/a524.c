1808e323a7c6216b96fc78605a5ad2264caed289
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 17:40:28 +0800

    AC (6ms, 96KB)

---
+++ C/a524.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int n;
+char unUse[10] = "012345678", output[10];
+
+void dfs(int now, char* oTop)
+{
+    if (now == n)
+    {
+        puts(output);
+        return;
+    }
+    for (int i = n; i; i--)
+    {
+        if (unUse[i])
+        {
+            *oTop = unUse[i] , unUse[i] = 0;
+            dfs(now + 1 , oTop + 1);
+            unUse[i] = *oTop;
+        }
+    }
+}
+
+int main()
+{
+    while (scanf("%d", &n) == 1)
+    {
+        output[n] = '\0';
+        dfs(0, output);
+    }
+    return 0;
+}
