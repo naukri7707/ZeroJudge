f95101537c019e21475904d0ed85d221756e375a
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 21:33:24 +0800

    AC (62ms, 332KB)

---
+++ C/a693.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int full[100010];
+
+int main()
+{
+    int n, m, f, t;
+    while (scanf(" %d %d", &n, &m) == 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            scanf(" %d", &full[i]);
+            full[i] += full[i - 1];
+        }
+        for (int i = 1; i <= m; i++)
+        {
+            scanf(" %d %d", &f, &t);
+            printf("%d\n", full[t] - full[f - 1]);
+        }
+    }
+    return 0;
+}
