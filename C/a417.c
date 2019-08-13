81d48ebeaf0c714b1ee06c37453dd4978b1952e8
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 16:41:00 +0800

    AC (66ms, 124KB)

---
+++ C/a417.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+
+int table[100][100];
+
+int main()
+{
+    int kase, n, m;
+    scanf(" %d", &kase);
+    while (kase--)
+    {
+        scanf(" %d %d", &n, &m);
+        int now = 1, end = n * n, sr = 0, sc = -1, er = n - 1, ec = n - 1, i, j;
+        while (now <= end)
+        {
+            for (j = ++sc; j <= ec; j++)
+                table[sr][j] = now++;
+            for (i = ++sr; i <= er; i++)
+                table[i][ec] = now++;
+            for (j = --ec; j >= sc; j--)
+                table[er][j] = now++;
+            for (i = --er; i >= sr; i--)
+                table[i][sc] = now++;
+        }
+        if (m == 1)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    printf("%5d", table[i][j]);
+                putchar('\n');
+            }
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    printf("%5d", table[j][i]);
+                putchar('\n');
+            }
+        }
+    }
+    return 0;
+}
