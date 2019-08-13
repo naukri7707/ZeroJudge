cf352f5549d686b1b8646cf8efc9c458e74b1c07
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-13 15:58:26 +0800

    AC (0.1s, 100KB)

---
+++ C/a414.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int n, count;
+    while (scanf(" %d", &n) && n)
+    {
+        count = 0;
+        while (n & 1)
+            count++, n >>= 1;
+        printf("%d\n", count);
+    }
+    return 0;
+}
