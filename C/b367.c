34b4724be71b19e0a20cd2a260ad37087bd01437
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-14 22:40:49 +0800

    AC (2ms, 104KB)

---
+++ C/b367.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int kase, n, m, s, e, list[150];
+    scanf(" %d", &kase);
+    while (kase--)
+    {
+        scanf(" %d %d", &n, &m);
+        s = 0, e = n * m - 1;
+        for (int i = 0; i <= e; i++)
+            scanf(" %d", &list[i]);
+        while (list[s] == list[e])
+        {
+            if (s > e)
+                break;
+            s++, e--;
+        }
+        puts(s < e ? "keep defending" : "go forward");
+    }
+    return 0;
+}
