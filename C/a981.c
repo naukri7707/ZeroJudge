8caaef31fb5385c65c3d9dd45d8ceca0a1bc06f2
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-14 11:58:20 +0800

    AC (0.2s, 96KB)

---
+++ C/a981.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int n, m, next, list[50], output[50];
+char noAns;
+
+int cmp(const int* lhs, const int* rhs)
+{
+    return *lhs - *rhs;
+}
+
+void dfs(int sum, int start, int depth)
+{
+    for (int i = start; i < n; i++)
+    {
+        next = sum + list[i];
+        if (next >= m)
+        {
+            if (next == m)
+            {
+                for (int j = 0; j < depth; j++)
+                    printf("%d ", output[j]);
+                printf("%d\n", list[i]);
+                noAns = 0;
+            }
+            if (list[i] < list[i + 1])
+                return;
+        }
+        output[depth] = list[i];
+        if (next + list[i] <= m)
+            dfs(next, i + 1, depth + 1);
+    }
+}
+
+int main()
+{
+    while (scanf(" %d %d", &n, &m) == 2)
+    {
+        noAns = 1;
+        for (int i = 0; i < n; i++)
+            scanf(" %d", &list[i]);
+        qsort(list, n, sizeof(int), cmp);
+        dfs(0, 0, 0);
+        if (noAns)
+            puts("-1");
+    }
+    return 0;
+}
