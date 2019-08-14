702ba710f98038e165a72d439167ba85d74d30f3
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-14 10:53:35 +0800

    AC (2ms, 112KB)

---
+++ C/a915.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#define MAX 1000
+
+typedef struct node
+{
+    int x, y;
+}Point;
+
+Point list[MAX], * sort[MAX];
+
+int cmp(const Point** lhs, const Point** rhs)
+{
+    return (*lhs)->x == (*rhs)->x ? (*lhs)->y - (*rhs)->y : (*lhs)->x - (*rhs)->x;
+}
+
+int main()
+{
+    for (int i = 0; i < MAX; i++)
+        sort[i] = &list[i];
+    int n;
+    while (scanf(" %d", &n) == 1)
+    {
+        for (int i = 0; i < n; i++)
+            scanf(" %d %d", &list[i].x, &list[i].y);
+        qsort(sort, n, sizeof(Point*), cmp);
+        for (int i = 0; i < n; i++)
+            printf("%d %d\n", sort[i]->x, sort[i]->y);
+    }
+    return 0;
+}
