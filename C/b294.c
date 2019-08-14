57095963d2a9b6e982e2d42ce9ba1a29ce147a32
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-14 22:17:51 +0800

    AC (2ms, 96KB)

---
+++ C/b294.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n , num , sum;
+    while (scanf(" %d", &n) == 1)
+    {
+        sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            scanf(" %d", &num);
+            sum += num * i;
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
