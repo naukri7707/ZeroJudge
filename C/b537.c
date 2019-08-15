c2502f41163edb89eab39354cb02d2bb1009dd80
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-15 16:34:18 +0800

    AC (2ms, 68KB)

--- C/b537.c
+++ C/b537.c
@@ -1,37 +1,24 @@
 ﻿#include <stdio.h>
-#define SWAP(x, y) x^=(y^=(x^=y))
-
-inline int gcd(int n, int m)
-{
-    while (n)
-    {
-        m %= n;
-        SWAP(m, n);
-    }
-    return m;
-}
 
 long long getK(int a, int b)
 {
-    if (a < b)
+    if (b < a)
     {
-        return a ? getK(b, a) + 1 : -b;
+        int shift = a / b;
+        a %= b;
+        return a ? (getK(b, a) + 1) << shift : 1LL << (shift - 1);
     }
     else
     {
-        int tmp = a / b;
-        long long res = getK(a - b * tmp, b);
-        return res > 0 ? res << tmp : -res << (tmp - 1);
+        return a == b ? 1 : getK(b, a) + 1;
     }
 }
 
 int main()
 {
-    int a, b, g;
+    int a, b;
     while (scanf(" %d %d", &a, &b) == 2)
     {
-        while ((g = gcd(a, b)) > 1)
-            a /= g, b /= g;
         printf("%lld\n", getK(a, b));
     }
     return 0;
