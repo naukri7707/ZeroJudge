0d8556b3e098b9ad16848dad30571754ddd07762
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-15 13:23:13 +0800

    AC (6ms, 608KB)

---
+++ C/b512.c
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Hash
+{
+    unsigned int Key;
+    int Value;
+    struct Hash* Next;
+}Hash;
+
+Hash HashTable[1024];
+
+inline void AddHash(int key, int value)
+{
+    Hash* now = &HashTable[key & 1023];
+    while (now->Key != key)
+    {
+        if (now->Next == NULL)
+        {
+            now = now->Next = malloc(sizeof(Hash));
+            now->Key = key;
+            now->Next = NULL;
+            now->Value = -value;
+            return;
+        }
+        now = now->Next;
+    }
+    now->Value *= -value;
+}
+
+int getInner()
+{
+    int res = 0;
+    Hash* now, * tmp;
+    for (int i = 0; i < 1024; i++)
+    {
+        if (HashTable[i].Next != NULL)
+        {
+            now = HashTable[i].Next;
+            do
+            {
+                if (now->Value > 0)
+                    res += now->Value;
+                tmp = now;
+                now = now->Next;
+                free(tmp);
+            } while (now != NULL);
+            HashTable[i].Next = NULL;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n, m, v = 1;
+    while (scanf(" %d:%d", &n, &m) == 2)
+    {
+        if (n + m)
+            AddHash(n, m);
+        else if (v)
+            v = 0;
+        else
+            printf("%d\n", getInner()), v = 1;
+    }
+    return 0;
+}
