a4138e6f50f508cfe9d62c40faefa65f7c485d89
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-18 03:11:28 +0800

    AC (2ms, 88KB)

---
+++ C/b558.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#define MAX 500
+
+int list[MAX - 1] = { 0,1 };
+
+int main()
+{
+    int n;
+    for (int i = 1; i < MAX; i++)
+        list[i + 1] = list[i] + i;
+    while (scanf(" %d", &n) == 1)
+    {
+        printf("%d\n", list[n]);
+    }
+    return 0;
+}
