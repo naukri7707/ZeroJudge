a3a1626ccef3bf82f7115477b8276c649020ae6e
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-18 01:22:55 +0800

    AC (2ms, 96KB)

--- C/b538.c
+++ C/b538.c
@@ -1,44 +1,54 @@
-﻿#pragma once
-#ifndef INC_NAUKRI
-#define INC_NAUKRI
+﻿#include <stdio.h>
+#define SWAP(x, y) x^=(y^=(x^=y))
 
-#pragma warning (disable : 4996)
+int gcd(int m, int n)
+{
+    while (n)
+    {
+        m %= n;
+        SWAP(m, n);
+    }
+    return m;
+}
 
-#ifndef CMPMODE
-// 0 = 隨意 (皆為隨意則不進行比較)
-// 1 = 不進行比較
-// 2 = 使用 compare.txt 儲存的答案比較
-// 3 = 使用 compare.c   計算的答案比較 (只有在 input 是檔案時才能執行)
-// 一一一一一一一一一一一一一一一一一一一
-// main() 的 return 也可以進行選擇，且若非隨意會優先使用 return 的定義。
-#define CMPMODE 2
-#endif
-
-// IO Mode
-#if !(defined(F_C) || defined(F_TC) || defined(T_T)  || defined(T_F)  || defined(T_TF) || defined(F_T) || defined(F_F) || defined(F_TF))
-// 輸入 => 輸出
-// T = 終端機 (Terminal)
-// F = 檔案 (File: output.txt)
-// C = 比對檔案 (File: compare.txt)
-// 若有多重輸出則以 T > F or C 為順序排列
-#define F_TF
-#endif
-
-#include <naukri/ntdio.h>
-
-#ifndef main
-// Main Wrapper
-#define main _entry
-#endif // !main
-
-int main(
-    void
-);
-
-#endif
-
-//compare.c Header (刪到的時用這個復原)
-#pragma region Mod Define
-//#define main _compare
-//#define F_TC
-#pragma endregion
+int main()
+{
+    int a, b, c, d, e, f, g;
+    char op;
+    while (scanf(" %d %d %d %d %c", &a, &b, &c, &d, &op) == 5)
+    {
+        switch (op)
+        {
+        case '+':
+            e = a * d + c * b;
+            f = b * d;
+            break;
+        case '-':
+            e = a * d - c * b;
+            f = b * d;
+            break;
+        case '*':
+            e = a * c;
+            f = b * d;
+            break;
+        case '/':
+            e = a * d;
+            f = b * c;
+            break;
+        default:
+            break;
+        }
+        g = gcd(e, f);
+        e /= g, f /= g;
+        if (f < 0)
+        {
+            e = -e;
+            f = -f;
+        }
+        if (f == 1)
+            printf("%d\n", e);
+        else
+            printf("%d/%d\n", e, f);
+    }
+    return 0;
+}
