fdbc3547f2116ed3b6dad4647ed17648d6455606
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-18 16:56:37 +0800

    AC (2ms, 72KB)

---
+++ C/b572.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int kase, h1, m1, h2, m2, t;
+    scanf("%d\n", &kase);
+    while (kase--)
+    {
+        scanf(" %d %d %d %d %d", &h1, &m1, &h2, &m2, &t);
+        m1 += h1 * 60 + t;
+        m2 += h2 * 60;
+        puts(m1 > m2 ? "No" : "Yes");
+    }
+    return 0;
+}
