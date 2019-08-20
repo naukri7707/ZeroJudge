4bb59b78c22beded5e6f6c9c62e662e80223747f
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-20 15:05:19 +0800

    AC (2ms, 100KB)

---
+++ C/b603.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#define SWAP(x, y) x^=(y^=(x^=y))
+
+inline int gcd(int m, int n)
+{
+    while (n)
+    {
+        m %= n;
+        SWAP(m, n);
+    }
+    return m;
+}
+
+int main()
+{
+    int  x1, x2, y1, y2, g, a, b, c, d;
+    while (scanf(" %d %d %d %d", &x1, &x2, &y1, &y2) == 4)
+    {
+        a = (y1 - x1) * (y1 - x1);
+        b = (y2 - x2);
+        g = gcd(a, b);
+        b /= g;
+        a /= g;
+        c = -2 * b * x1;
+        d = b * x1 * x1 + a * x2;
+        printf("%dy = %dx^2 + %dx + %d\n", a, b, c, d);
+    }
+    return 0;
+}
