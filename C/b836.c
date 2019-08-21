e314884156d636e575fe3aa837fa0a775d498805
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 16:17:13 +0800

    AC (2ms, 88KB)

---
+++ C/b836.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    unsigned int c;
+    while (scanf(" %d %d", &n, &m) == 2)
+    {
+        c = 1;
+        if (!m)
+            n = 0;
+        while (n > 0)
+        {
+            n -= c;
+            c += m;
+        }
+        puts(n ? "No Stop!!" : "Go Kevin!!");
+    }
+    return 0;
+}
