f0b79fb6f4785cf1ab5e860c1f3d9e98656ed11d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 14:51:47 +0800

    AC (3ms, 84KB)

---
+++ C/b759.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+char s[2000010];
+
+int main()
+{
+    gets(s);
+    int len = strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        puts(s + i);
+        s[len + i] = s[i];
+    }
+    return 0;
+}
