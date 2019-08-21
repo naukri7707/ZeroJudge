a4cc61c09e4d2cf4a37f6251d19f66e28aac26dc
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 17:00:15 +0800

    AC (2ms, 112KB)

---
+++ C/b884.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, x, y, yee;
+    scanf(" %d", &n);
+    while (n--)
+    {
+        scanf(" %d %d", &x, &y);
+        yee = 100 - x - y;
+        if (yee <= 0) puts("evil!!");
+        else if (yee <= 30) puts("sad!");
+        else if (yee <= 60) puts("hmm~~");
+        else if (yee < 100) puts("Happyyummy");
+        else puts("evil!!");
+    }
+    return 0;
+}
