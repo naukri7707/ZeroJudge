df62c6edf5143237bd9311cdd1b46ffb7908205d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 20:46:02 +0800

    AC (66ms, 176KB)

--- C/b897.c
+++ C/b897.c
@@ -5,11 +5,11 @@ int main()
 {
     register double log;
     int n, k, max;
-    while (scanf(" %d %d", &k, &n) == 2)
+    while (scanf(" %d %d", &n, &k) == 2)
     {
-        log = 0, max = n < k - n ? n : k - n;
+        log = 0, max = k < n - k ? k : n - k;
         for (int i = 0; i < max; i++)
-            log += log10(k - i) - log10(i + 1);
+            log += log10(n - i) - log10(i + 1);
         printf("%d\n", (int)log + 1);
     }
     return 0;
