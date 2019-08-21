44dab4b2c9f228b510791c560dec0da22b0c1378
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 18:45:48 +0800

    AC (66ms, 176KB)

---
+++ C/b897.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    register double log;
+    int n, k, max;
+    while (scanf(" %d %d", &k, &n) == 2)
+    {
+        log = 0, max = n < k - n ? n : k - n;
+        for (int i = 0; i < max; i++)
+            log += log10(k - i) - log10(i + 1);
+        printf("%d\n", (int)log + 1);
+    }
+    return 0;
+}
