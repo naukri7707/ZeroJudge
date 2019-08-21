2b9d1cf7b7f6ef8c9c2778e2f88dc588ac68b855
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 14:04:27 +0800

    AC (2ms, 104KB)

---
+++ C/b758.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int h, m;
+    while (scanf(" %d %d", &h, &m) == 2)
+    {
+        m += h * 60 + 150;
+        h = m / 60, m = m % 60;
+        printf("%02d:%02d\n", h < 24 ? h : h - 24, m);
+    }
+    return 0;
+}
