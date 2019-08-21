a72ac954e21c02b8eea1e23c274ec22a44b29bf5
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 22:22:30 +0800

    AC (1.7s, 116KB)

---
+++ C/b936.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+long long n, m;
+
+long long dfs(long long piece)
+{
+    if ((piece >> 1) < m)
+        return 1;
+    if (piece & 1)
+        return dfs((piece + 1) >> 1) + dfs((piece - 1) >> 1);
+    else
+        return dfs(piece >> 1) << 1;
+}
+
+int main()
+{
+    while (scanf(" %lld %lld", &n, &m) == 2)
+    {
+        printf("%lld\n", n < m ? 0 : dfs(n));
+    }
+    return 0;
+}
