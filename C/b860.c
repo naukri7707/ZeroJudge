048c9a060aba0ddea6b62cdcbfbd8afc19d42adf
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 16:43:17 +0800

    AC (2ms, 120KB)

---
+++ C/b860.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int candy, weedle, kakuna, count, min;
+    while (scanf(" %d %d", &candy, &weedle) == 2)
+    {
+        kakuna = count = 0;
+        do
+        {
+            min = candy / 12;
+            if (weedle < min)
+                min = weedle;
+            if (min)
+            {
+                candy -= 11 * min;
+                weedle -= min;
+                kakuna += min;
+                count += min;
+            }
+            candy += kakuna;
+            kakuna = 0;
+            if (candy < 12 && weedle)
+                candy++, weedle--;
+        } while (weedle);
+        printf("%d\n", count);
+    }
+    return 0;
+}
