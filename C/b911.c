84eb01da6114a693e32370e67bca94684bc55062
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 21:24:07 +0800

    AC (2ms, 104KB)

---
+++ C/b911.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    unsigned long long n;
+    int count;
+    while (scanf(" %llu", &n) == 1)
+    {
+        count = 0;
+        while (n)
+        {
+            n >>= 1;
+            count++;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
