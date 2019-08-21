9086de55a7742eecfb61eca6472d1d7b82bb0560
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 22:44:40 +0800

    AC (0.3s, 7.7MB)

---
+++ C/b938.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#define MAX 1000010
+
+typedef struct Node
+{
+    int Next;
+    char Alive;
+}Player;
+
+Player list[MAX];
+int n, m;
+
+int main()
+{
+    while (scanf(" %d %d", &n, &m) == 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            list[i].Alive = 1;
+            list[i].Next = i + 1;
+        }
+        list[n + 1].Alive = 0;
+        for (int i = 0, k; i < m; i++)
+        {
+            scanf(" %d", &k);
+            if (list[k].Alive && list[list[k].Next].Alive)
+            {
+                printf("%d\n", list[k].Next);
+                list[list[k].Next].Alive = 0;
+                list[k].Next = list[list[k].Next].Next;
+            }
+            else
+            {
+                puts("0u0 ...... ?");
+            }
+        }
+    }
+    return 0;
+}
