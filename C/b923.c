1697c61d568531726d4288abfd5046572e678c57
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-21 21:24:15 +0800

    AC (2ms, 80KB)

---
+++ C/b923.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int stack[100001];
+
+int main()
+{
+    int n, sTop;
+    char cmd;
+    while (scanf(" %d", &n) == 1)
+    {
+        sTop = 0;
+        while (n--)
+        {
+            scanf(" %c", &cmd);
+            if (cmd == '1')
+                sTop--;
+            else if (cmd == '2')
+                printf("%d\n", stack[sTop]);
+            else
+                scanf(" %d", &stack[++sTop]);
+        }
+    }
+    return 0;
+}
