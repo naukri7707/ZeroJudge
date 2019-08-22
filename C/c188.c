8694fe8e168224550e6969ca7763fd37d4e9bfb6
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-22 14:44:43 +0800

    AC (2ms, 80KB)

---
+++ C/c188.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#define MAX 201
+
+int main()
+{
+    unsigned long long int list[MAX] = { 1 };
+    int n;
+    for (int i = 1; i < MAX; i++)
+    {
+        for (int j = i; j < MAX; j++)
+            list[j] += list[j - i];
+    }
+    while (scanf(" %d", &n) == 1)
+    {
+        printf("%llu\n", list[n]);
+    }
+    return 0;
+}
