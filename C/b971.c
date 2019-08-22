7ee21f2159693f9c530b54a1f1e5633439b1f67a
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-22 10:44:46 +0800

    AC (3ms, 96KB)

---
+++ C/b971.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int s, e, d;
+    while (scanf(" %d %d %d", &s, &e, &d) == 3)
+    {
+        for (; s != e; s += d)
+            printf("%d ", s);
+        printf("%d\n", e);
+    }
+    return 0;
+}
