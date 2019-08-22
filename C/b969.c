5ccfa6d39367c38bfe441b106f27d4eeb23c0c0d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-22 10:05:21 +0800

    AC (2ms, 108KB)

---
+++ C/b969.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define MAX 10000
+
+char name[MAX], hello[MAX], * hs, * h;
+
+int main()
+{
+    fgets(name, MAX, stdin);
+    fgets(hello, MAX, stdin);
+    h = hello + strlen(hello);
+    *h++ = ',', * h++ = ' ';
+    hs = h;
+    for (int i = 0; name[i]; i++)
+    {
+        if (name[i] == ' ')
+            * h = '\0', puts(hello), h = hs;
+        else
+            *h++ = name[i];
+    }
+    *h = '\0', puts(hello);
+    return 0;
+}
