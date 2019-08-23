2e4e73c5b8c03815c5d3ccb809866ac059473500
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-23 22:42:01 +0800

    AC (2ms, 120KB)

---
+++ C/c317.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#define SWAP(x, y) (x)^=((y)^=((x)^=(y)))
+
+int main()
+{
+    int n, target, coin1, coin2, i, sum, count;
+    while (scanf(" %d", &n) == 1)
+    {
+        while (n--)
+        {
+            scanf(" %d %d %d", &target, &coin1, &coin2);
+            if (coin1 < coin2)
+                SWAP(coin1, coin2);
+            i = target / coin1, count = -1;
+            sum = coin1 * (i + 1);
+            for (; i >= 0; i--)
+            {
+                sum -= coin1;
+                if ((target - sum) % coin2 == 0)
+                {
+                    count = i + (target - sum) / coin2;
+                    break;
+                }
+            }
+            printf("%d\n", count);
+        }
+    }
+    return 0;
+}
