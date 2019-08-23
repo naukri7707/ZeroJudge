8defd8b8ff3e1016b09479ec53636e4ca4a26a03
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-23 20:56:45 +0800

    AC (2ms, 108KB)

---
+++ C/c315.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int  n, x, y, a, b;
+    while (scanf(" %d", &n) == 1)
+    {
+        x = y = 0;
+        for (int i = 0; i < n; i++)
+        {
+            scanf(" %d %d", &a, &b);
+            switch (a)
+            {
+            case 0:
+                y += b;
+                break;
+            case 1:
+                x += b;
+                break;
+            case 2:
+                y -= b;
+                break;
+            default:
+                x -= b;
+                break;
+            }
+        }
+        printf("%d %d\n", x, y);
+    }
+    return 0;
+}
