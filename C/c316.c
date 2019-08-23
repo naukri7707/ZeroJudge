f65ff60ee8e5bf63d1e234be337b87cddc61d6e1
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-23 21:08:44 +0800

    AC (5ms, 152KB)

---
+++ C/c316.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+typedef struct Point
+{
+    int X, Y;
+}Point;
+
+Point list[1010], ans;
+
+int main()
+{
+    double best, tmp;
+    int n;
+    while (scanf(" %d", &n) == 1)
+    {
+        best = 0;
+        for (int i = 0; i < n; i++)
+        {
+            scanf(" %d %d", &list[i].X, &list[i].Y);
+            for (int j = 0; j < i; j++)
+            {
+                tmp = sqrt((list[i].X - list[j].X) * (list[i].X - list[j].X) + (list[i].Y - list[j].Y) * (list[i].Y - list[j].Y));
+                if (best < tmp)
+                {
+                    best = tmp;
+                    ans.X = j;
+                    ans.Y = i;
+                }
+            }
+        }
+        printf("%d %d\n", ans.X, ans.Y);
+    }
+    return 0;
+}
