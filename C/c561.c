d0ed06659a7a234486a1f80569ef21006e4d1923
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 20:47:05 +0800

    AC (12ms, 124KB)

---
+++ C/c561.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int n, tmp, convert, best;
+
+int main()
+{
+    scanf(" %d", &n);
+    while (n--)
+    {
+        convert = 0;
+        scanf(" %d", &tmp);
+        while (tmp)
+        {
+            convert = convert * 10 + (tmp % 10);
+            tmp /= 10;
+        }
+        if (best < convert)
+            best = convert;
+    }
+    printf("%d\n", best);
+    return 0;
+}
