e8170e9f8216db171bd11144a42c8ba5d27f459f
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 11:57:21 +0800

    AC (2ms, 188KB)

---
+++ C/c440.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+char input[100010];
+
+int main()
+{
+    long long ans = 0;
+    int sumQ = 0, leftQ = 0;
+    gets(input);
+    for (int i = 0; input[i]; i++)
+    {
+        if (input[i] == 'Q')
+            sumQ++, ans += leftQ;
+        else if (input[i] == 'A')
+            leftQ += sumQ;
+    }
+    printf("%lld", ans);
+    return 0;
+}
