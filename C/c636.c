2df61136461e137ca3abc3b20bc1e4107b425976
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 21:31:47 +0800

    AC (1ms, 76KB)

---
+++ C/c636.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int n;
+char Zodiac[][4] = { "豬", "鼠", "牛", "虎", "兔", "龍", "蛇", "馬", "羊", "猴", "雞", "狗" };
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        puts(Zodiac[(n < 0 ? n + 121 : n) % 12]);
+    }
+    return 0;
+}
