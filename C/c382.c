d9387227dc5b3aff76e6c3276ed4c1e75b6f120a
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 10:16:18 +0800

    AC (2ms, 76KB)

---
+++ C/c382.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    char s;
+    while (scanf(" %d %c %d", &a, &s, &b) == 3)
+    {
+        switch (s)
+        {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        default:
+            printf("%d\n", a / b);
+            break;
+        }
+    }
+    return 0;
+}
