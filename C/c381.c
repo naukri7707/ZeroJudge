1656797e7b8e68df042f260045ea7c6b744f258d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 10:01:09 +0800

    AC (13ms, 4.9MB)

---
+++ C/c381.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+char list[60000010], ans[101], * input;
+
+int main()
+{
+    int n, m, tmp;
+    while (scanf(" %d %d", &n, &m) == 2 && n + m)
+    {
+        getchar();
+        input = list;
+        int length = 0;
+        for (int i = 0; i < n; i++)
+        {
+            gets(input);
+            input += strlen(input);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            scanf(" %d", &tmp);
+            ans[i] = list[tmp - 1];
+        }
+        ans[m] = 0;
+        puts(ans);
+    }
+    return 0;
+}
