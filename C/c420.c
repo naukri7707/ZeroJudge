ccff9a200fd714799c9e697c8bdc8c369e05b7ab
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 10:48:09 +0800

    AC (2ms, 100KB)

---
+++ C/c420.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+char s[200];
+
+int main()
+{
+    int n;
+    scanf(" %d", &n);
+    for (int i = n * 2 - 2; i >= 0; i--)
+        s[i] = '_';
+    n--;
+    for (int i = 0; i <= n; i++)
+    {
+        s[n - i] = s[n + i] = '*';
+        puts(s);
+    }
+    return 0;
+}
