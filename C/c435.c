d9e7673f3e77d2a70556004f4305fe80d6d68880
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 11:18:43 +0800

    AC (11ms, 104KB)

---
+++ C/c435.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, max, min, tmp, best;
+    while (scanf(" %d", &n) == 1)
+    {
+        scanf(" %d", &min);
+        best = 0, max = min;
+        while (--n)
+        {
+            scanf(" %d", &tmp);
+            if (tmp < min)
+                min = tmp;
+            else if (tmp > max)
+            {
+                if (max - min > best)
+                    best = max - min;
+                max = min = tmp;
+            }
+        }
+        printf("%d\n", best < max - min ? max - min : best);
+    }
+    return 0;
+}
