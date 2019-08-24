e6bf55608eebe3b97d0e7595dd9a565f8345665a
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 10:37:55 +0800

    AC (2ms, 104KB)

---
+++ C/c419.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+char s[101];
+
+int main()
+{
+    int n;
+    scanf(" %d", &n);
+    for (int i = 0; i < n; i++)
+        s[i] = '_';
+    for (int i = n - 1; i >= 0; i--)
+    {
+        s[i] = '*';
+        puts(s);
+    }
+    return 0;
+}
