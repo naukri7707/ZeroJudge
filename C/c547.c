f8268828bbe1bd7c7a4acd1c7ae9a3e1d9e3f8d3
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 17:57:56 +0800

    AC (4ms, 124KB)

---
+++ C/c547.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#define Mod 1000000007
+
+int list[10001] = { 1, 1 };
+
+int main()
+{
+    int n;
+    for (int i = 2; i <= 10000; i++)
+        list[i] = (list[i - 1] + list[i - 2]) % Mod;
+    while (scanf("%d", &n) == 1)
+    {
+        printf("%d\n", list[n]);
+    }
+    return 0;
+}
