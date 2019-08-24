26104ab74f02cab84684544a1cff251384c06c5c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 09:10:31 +0800

    AC (2ms, 112KB)

---
+++ C/c350.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, k, w, m, sum, gap;
+    scanf(" %d %d %d", &n, &k, &w);
+    sum = n, gap = k - w;
+    for (int i = n / k; i; i = n / k)
+        sum += w * i, n -= gap * i;
+    printf("%d\n", sum);
+    return 0;
+}
