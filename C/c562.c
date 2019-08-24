24ee0a688288863c1d53a8304b5deaa7151d5d3d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 20:59:11 +0800

    AC (5ms, 96KB)

---
+++ C/c562.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int ans;
+char list[11];
+
+int main()
+{
+    while (scanf(" %s", &list) == 1)
+    {
+        ans = 0;
+        for (int i = 0; list[i]; i++)
+        {
+            if (list[i] == '0') ans++;
+            else if (list[i] == '6') ans++;
+            else if (list[i] == '8') ans += 2;
+            else if (list[i] == '9') ans++;
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
