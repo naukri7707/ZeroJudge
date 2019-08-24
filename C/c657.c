671270226e28e03eb833c2e9e768f03f88cfcb58
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 21:46:53 +0800

    AC (2ms, 116KB)

---
+++ C/c657.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+char input[10000], curr, bestC;
+int count, best;
+
+int main()
+{
+    while (scanf(" %s", input) == 1)
+    {
+        count = best = 0;
+        for (int i = 0; input[i]; i++)
+        {
+            if (input[i] == curr)
+                count++;
+            else
+            {
+                if (best < count)
+                    best = count, bestC = curr;
+                count = 1, curr = input[i];
+            }
+        }
+        if (best < count)
+            best = count, bestC = curr;
+        printf("%c %d\n", bestC, best);
+    }
+    return 0;
+}
