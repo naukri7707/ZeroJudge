0be60246c592f0448706f221eb3f8bcafd05d1c4
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 11:02:55 +0800

    AC (0.2s, 120KB)

---
+++ C/c431.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int n, tmp, list[101];
+
+int main()
+{
+    scanf(" %d", &n);
+    while (n--)
+    {
+        scanf(" %d", &tmp);
+        list[tmp]++;
+    }
+    for (int i = 1; i <= 100; i++)
+    {
+        while (list[i]--)
+            printf("%d ", i);
+    }
+    return 0;
+}
