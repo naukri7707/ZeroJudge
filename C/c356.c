27b54a854b70f75dcad7f3cf1d5271d4a9a75a8c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-24 09:37:21 +0800

    AC (11ms, 112KB)

---
+++ C/c356.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#define MAX 5005
+
+char s[MAX], ans[MAX];
+
+int main()
+{
+    int n, np;
+    scanf(" %d", &n);
+    getchar();
+    np = n + 2;
+    for (int i = 0; i < n; i++)
+    {
+        fgets(s, np, stdin);
+        ans[i] = s[0];
+    }
+    ans[n] = 0;
+    puts(ans);
+    return 0;
+}
