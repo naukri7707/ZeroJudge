a5a2f0f9d165e326a2d1ae0f5e4c67e70f2a9981
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-25 12:50:25 +0800

    AC (2ms, 108KB)

---
+++ C/c659.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#define SIZE 10000
+
+char input[SIZE], output[SIZE], conj[100], cLen, i, oLen;
+
+int main()
+{
+    gets(input);
+    for (i = 0; input[i] != ' '; i++)
+        conj[cLen++] = input[i];
+    i++;
+    for (; input[i]; i++)
+    {
+        output[oLen++] = input[i];
+        if (input[i] == ' ')
+        {
+            for (int j = 0; j < cLen; j++)
+                output[oLen++] = conj[j];
+            output[oLen++] = ' ';
+        }
+    }
+    puts(output);
+    return 0;
+}
