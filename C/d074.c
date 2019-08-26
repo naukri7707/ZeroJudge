fbc4a5702e0732c1a748d8df1f29f7f17b80af51
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-27 01:30:45 +0800

    AC (2ms, 124KB)

---
+++ C/d074.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int n, tmp, max = 0;
+    scanf(" %d", &n);
+    while (n--)
+    {
+        scanf("%d", &tmp);
+        if (max < tmp)
+            max = tmp;
+    }
+    printf("%d", max);
+    return 0;
+}
