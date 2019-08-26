792892ec1e88c0b21ea0065953033c7e4c27958c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-26 14:09:24 +0800

    AC (2ms, 92KB)

---
+++ C/c929.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#define SIZE 2000
+
+int oLen;
+char spilt[SIZE], input[SIZE], output[SIZE];
+
+inline int strspt(int* idx, char* spilt)
+{
+    int i = 0;
+    for (; spilt[i]; i++)
+    {
+        if (input[*idx + i] != spilt[i] || !input[*idx + i])
+            return 0;
+    }
+    *idx += i - 1;
+    return 1;
+}
+
+int main()
+{
+    gets(spilt), gets(input);
+    for (int i = 0; input[i]; i++)
+    {
+        if (strspt(&i, spilt))
+            output[oLen++] = '\n';
+        else
+            output[oLen++] = input[i];
+    }
+    puts(output);
+    return 0;
+}
