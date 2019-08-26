ccd9efbc46439dd191d0ddc82ed3fca22f87a619
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-27 01:15:51 +0800

    AC (2ms, 108KB)

---
+++ C/d072.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int kase, y;
+    scanf(" %d", &kase);
+    for (int i = 1; i <= kase; i++)
+    {
+        scanf(" %d", &y);
+        if (!(y % 400) || !(y % 4) && (y % 100))
+            printf("Case %d: a leap year\n", i);
+        else
+            printf("Case %d: a normal year\n", i);
+    }
+    return 0;
+}
