44867f276bdc59e3ff277437de3629872efdab94
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-26 23:06:21 +0800

    AC (2ms, 100KB)

---
+++ C/d018.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+double num, sum;
+int idx;
+char input[10000], * tok;
+
+int main()
+{
+    while (gets(input) != NULL)
+    {
+        sum = 0, tok = strtok(input, " ");
+        while (tok != NULL)
+        {
+            sscanf(tok, "%d:%lf", &idx, &num);
+            sum += idx & 1 ? num : -num;
+            tok = strtok(NULL, " ");
+        }
+        printf("%g\n", sum);
+    }
+    return 0;
+}
