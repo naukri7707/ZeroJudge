1fac35009dc6ba2f85c810a3f4c0ad8964e7fa79
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-26 19:01:06 +0800

    AC (2ms, 72KB)

---
+++ C/d010.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int n, k;
+    while (scanf(" %d", &n) == 1)
+    {
+        k = 0;
+        for (int i = n - 1; i > 0; i--)
+        {
+            if (!(n % i))
+                k = k + i;
+        }
+        if (k < n)
+            puts("虧數");
+        else if (k > n)
+            puts("盈數");
+        else
+            puts("完全數");
+    }
+    return 0;
+}
