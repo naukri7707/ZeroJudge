7646bf30508768ac540f5de6760f54fa2911413e
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-27 00:35:53 +0800

    AC (2ms, 96KB)

---
+++ C/d067.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int y;
+    scanf(" %d", &y);
+    if (!(y % 400) || !(y % 4) && (y % 100))
+        puts("a leap year");
+    else
+        puts("a normal year");
+    return 0;
+}
