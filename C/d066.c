20f41be4cd23f8917a7c4a13668e923a935954fd
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-27 00:30:55 +0800

    AC (2ms, 88KB)

---
+++ C/d066.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int h, m;
+    scanf(" %d %d", &h, &m);
+    if (h == 7 && m >= 30 || h > 7 && h < 17)
+        puts("At School");
+    else
+        puts("Off School");
+    return 0;
+}
