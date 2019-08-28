b689924cb91c2874f8846416b7f0ceebace97257
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-28 10:46:24 +0800

    AC (2ms, 108KB)

---
+++ C/d166.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#define MAX 55
+
+char input[10000];
+
+inline char* getUInt(unsigned int* dst, char src[])
+{
+    while (*src < '0')
+        if (!*src++) return NULL;
+    *dst = *src ^ '0';
+    while (*++src >= '0')
+        * dst = (*dst << 3) + (*dst << 1) + (*src ^ '0');
+    return src;
+}
+
+int main()
+{
+    while (gets(input) && input[0] != '-')
+    {
+        char* st = input;
+        int ans[MAX] = { 0 }, tmp, j;
+        for (int i = 0; st = getUInt(&tmp, st); i++)
+        {
+            for (j = 0; tmp || ans[j]; j++)
+                if (!ans[j]) tmp--;
+            ans[j] = i + 1;
+        }
+        for (int i = 0; ans[i]; i++)
+            printf("%d ", ans[i]);
+        putchar('\n');
+    }
+    return 0;
+}
