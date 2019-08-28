98875c5fe5ee0fa513a7d9d90181d9dfe7f0737f
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-28 09:33:58 +0800

    AC (2ms, 100KB)

---
+++ C/d124.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+char input[10010];
+
+int main()
+{
+    int sum;
+    while (scanf(" %s", input) == 1)
+    {
+        sum = 0;
+        for (int i = 0; input[i]; i++)
+            sum += input[i] - '0';
+        puts(sum % 3 ? "no" : "yes");
+    }
+    return 0;
+}
