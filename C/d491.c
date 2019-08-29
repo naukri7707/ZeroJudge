6e088fcaf2983255d12a91ae7fdad6ab8feccbb6
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 14:40:41 +0800

    AC (2ms, 112KB)

---
+++ C/d491.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#define SWAP(x, y) (x)^=((y)^=((x)^=(y)))
+
+int main()
+{
+    int n, m;
+    scanf(" %d %d", &n, &m);
+    if (m < n) SWAP(n, m);
+    n += (n & 1), m -= (m & 1);
+    printf("%d\n", ((n + m) * ((m - n) / 2 + 1)) / 2);
+    return 0;
+}
