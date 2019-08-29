4d97e4b6b1caa9c74db56cc942b326be95bc0ff2
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 16:21:06 +0800

    AC (28ms, 128KB)

---
+++ C/d526.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int value;
+    struct Node* lhs, * rhs;
+}Node;
+
+inline Node* newNode(int value)
+{
+    Node* res = malloc(sizeof(Node));
+    res->value = value;
+    res->lhs = res->rhs = NULL;
+    return res;
+}
+
+inline void setNode(Node* now, int value)
+{
+    for (;;)
+    {
+        if (value < now->value)
+        {
+            if (now->lhs)
+                now = now->lhs;
+            else
+            {
+                now->lhs = newNode(value);
+                break;
+            }
+        }
+        else
+        {
+            if (now->rhs)
+                now = now->rhs;
+            else
+            {
+                now->rhs = newNode(value);
+                break;
+            }
+        }
+    }
+}
+
+void serchNode(Node* now)
+{
+    if (now)
+    {
+        printf("%d ", now->value);
+        serchNode(now->lhs);
+        serchNode(now->rhs);
+        free(now);
+    }
+}
+
+int main()
+{
+    int n, tmp;
+    while (scanf(" %d", &n) == 1)
+    {
+        Node root = { 0 };
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &tmp);
+            setNode(&root, tmp);
+        }
+        serchNode(root.rhs);
+        putchar('\n');
+    }
+    return 0;
+}
