b44a458abf6b4bd5821e0ae38dae653fd044a41b
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 14:21:55 +0800

    AC (2ms, 112KB)

---
+++ C/d489.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, b, c, s;
+    scanf(" %d %d %d", &a, &b, &c);
+    s = (a + b + c) / 2;
+    printf("%d\n", s * (s - a) * (s - b) * (s - c));
+    return 0;
+}
