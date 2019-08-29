ee38015dd6d1acfa52eeef9dccb3ef2c6548124b
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 11:06:13 +0800

    AC (3ms, 76KB)

---
+++ C/d471.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int n;
+char ans[16];
+
+void dfs(int lv)
+{
+    if (lv == n)
+    {
+        puts(ans);
+        return;
+    }
+    ans[lv] = '0';
+    dfs(lv + 1);
+    ans[lv] = '1';
+    dfs(lv + 1);
+}
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        ans[n] = 0;
+        dfs(0);
+    }
+    return 0;
+}
