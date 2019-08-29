5c89c9b3aa0f24101ffb37572b5ef0c54cb6a7dc
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 10:13:37 +0800

    AC (2ms, 76KB)

---
+++ C/d326.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int v, a, b;
+    while (scanf(" %d %d %d", &v, &a, &b) == 3)
+    {
+        v = b ? (v | (1 << a)) : (v & ~(1 << a));
+        for (int i = 31; ~i; i--)
+            putchar(((v >> i) & 1) + '0');
+        putchar('\n');
+    }
+    return 0;
+}
