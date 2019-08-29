6daaada9de400249ab49c20b5cbc014a7f3a24f9
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 14:32:09 +0800

    AC (2ms, 116KB)

---
+++ C/d490.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    scanf(" %d %d", &n, &m);
+    n += (n & 1), m -= (m & 1);
+    printf("%d\n", ((n + m) * ((m - n) / 2 + 1)) / 2);
+    return 0;
+}
