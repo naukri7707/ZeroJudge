d556bb0b0508b8feeef838f97056e93cce55b26c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 14:51:04 +0800

    AC (2ms, 104KB)

---
+++ C/d511.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const int* lhs, const int* rhs)
+{
+    return *lhs - *rhs;
+}
+
+int main()
+{
+    int list[3], ans = 0;
+    for (int h = 0; h < 5; h++)
+    {
+        for (int i = 0; i < 3; i++)
+            scanf(" %d", &list[i]);
+        qsort(list, 3, sizeof(int), cmp);
+        if (list[0] + list[1] > list[2]) ans++;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
