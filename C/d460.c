02ecbcee2cd34dad8bed39957e3fdfe70b20e66e
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-29 10:46:43 +0800

    AC (2ms, 104KB)

---
+++ C/d460.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", (n >= 6 && n < 12) * 590 + (n >= 12 && n < 18) * 790
+        + (n >= 18 && n < 60) * 890 + (n >= 60) * 399);
+    return 0;
+}
