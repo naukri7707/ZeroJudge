bbc536ea82d33cb799aa87a87575698eccdc44fc
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-30 16:48:19 +0800

    AC (2ms, 112KB)

---
+++ C/d532.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, m, cont = 0;
+    scanf(" %d %d", &n, &m);
+    for (int i = n; i <= m; i++)
+    {
+        if (!(i % 400) || (i % 100) && !(i % 4))
+            cont++;
+    }
+    printf("%d\n", cont);
+    return 0;
+}
