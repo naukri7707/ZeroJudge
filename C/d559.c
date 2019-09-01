720edee35c60d886d37f8cef712320af488e2b3a
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-01 10:50:49 +0800

    AC (1ms, 100KB)

---
+++ C/d559.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    while (scanf(" %d", &n) == 1)
+    {
+        printf("'C' can use printf(\"%%d\",n); to show integer like %d\n", n);
+    }
+    return 0;
+}
