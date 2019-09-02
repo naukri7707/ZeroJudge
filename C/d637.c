2b8ca569c6a8aac83f4b1e3e5259c50c244d1945
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 03:22:24 +0800

    AC (4ms, 116KB)

---
+++ C/d637.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int DP[101];
+
+int main()
+{
+    int n, a, b;
+    scanf(" %d", &n);
+    while (n--)
+    {
+        scanf(" %d %d", &a, &b);
+        for (int i = 100; i >= a; i--)
+        {
+            if (DP[i] < DP[i - a] + b)
+                DP[i] = DP[i - a] + b;
+        }
+    }
+    printf("%d", DP[100]);
+    return 0;
+}
