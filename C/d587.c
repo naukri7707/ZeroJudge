b3c405ec721e0c776abe429da061ed002f77b79d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-02 22:19:42 +0800

    AC (3ms, 116KB)

---
+++ C/d587.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int list[4];
+char outstr[4][3] = { "", "1 ", "2 " , "3 " };
+
+int main()
+{
+    int n, tmp;
+    scanf(" %d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %d", &tmp);
+        list[tmp]++;
+    }
+    for (int i = 1; i <= 3; i++)
+    {
+        while (list[i]--)
+            fputs(outstr[i], stdout);
+    }
+    return 0;
+}
