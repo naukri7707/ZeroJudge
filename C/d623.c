c4474da17b5d4c55af297ad48889bff1bf2e5c24
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-02 22:27:21 +0800

    AC (3ms, 64KB)

---
+++ C/d623.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    double a, b, c, d, det;
+    while (scanf("%lf %lf %lf %lf", &a, &b, &c, &d) == 4 && a + b + c + d)
+    {
+        det = a * d - b * c;
+        if (det)
+            printf("%.5lf %.5lf\n%.5lf %.5lf\n", d / det, -b / det, -c / det, a / det);
+        else
+            puts("cheat!");
+    }
+    return 0;
+}
