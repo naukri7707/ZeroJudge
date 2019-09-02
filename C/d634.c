a00b236db85190c88c8b9c7d0cb3c769c3d0ee8f
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-02 22:56:10 +0800

    AC (53ms, 2.7MB)

---
+++ C/d634.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char list[100000][11], * sort[100000];
+
+int cmp(const char** lhs, const char** rhs)
+{
+    return strcmp(*lhs, *rhs);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        sort[i] = list[i];
+    getchar();
+    for (int i = 0; i < n; i++)
+        gets(list[i]);
+    qsort(sort, n, sizeof(char**), cmp);
+    for (int i = 0; i < n; i++)
+        puts(sort[i]);
+    return 0;
+}
