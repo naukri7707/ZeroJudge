8c05ed62e2d6cdd5c0c6b877f22eb7d6c4c522db
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 00:08:32 +0800

    AC (2ms, 96KB)

---
+++ C/d636.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#define MOD 10007
+
+int flt(int a, int b)
+{
+    int res = 1, next = a % MOD;
+    for (int i = 0; i < 32; i++)
+    {
+        if (b & 1)
+            res = res * next % MOD;
+        next = next * next % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    int a, b;
+    scanf(" %d %d", &a, &b);
+    printf("%d\n", flt(a, b));
+    return 0;
+}
