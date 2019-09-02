a7d8de0216dee9c16873c7490e45a404c84df704
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-02 22:41:04 +0800

    AC (2ms, 112KB)

---
+++ C/d626.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+char map[110][110];
+
+void dfs(int r, int c)
+{
+    if (map[r][c] == '+' || !map[r][c])
+        return;
+    map[r][c] = '+';
+    dfs(r, c + 1);
+    dfs(r + 1, c);
+    dfs(r, c - 1);
+    dfs(r - 1, c);
+}
+
+int main()
+{
+    int n, r, c;
+    scanf(" %d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf(" %s", map[i] + 1);
+    scanf(" %d %d", &r, &c);
+    dfs(r + 1, c + 1);
+    for (int i = 1; i <= n; i++)
+        puts(map[i] + 1);
+    return 0;
+}
