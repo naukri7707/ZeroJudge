394da43045ff328e0121693846e1d0c3d0a464bd
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 18:30:03 +0800

    AC (2ms, 108KB)

---
+++ C/d693.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#define SWAP(x, y) (x)^=((y)^=((x)^=(y)))
+
+inline int lcm(long long m, int n)
+{
+    long long res = m * n;
+    while (n)
+    {
+        m %= n;
+        SWAP(m, n);
+    }
+    return res / m;
+}
+
+int main()
+{
+    int n, ans, tmp;
+    while (scanf(" %d %d", &n, &ans) == 2 && n)
+    {
+        while (--n)
+        {
+            scanf(" %d", &tmp);
+            ans = lcm(ans, tmp);
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
