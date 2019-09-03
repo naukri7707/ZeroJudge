40bb5d66831284087d61d2af55a8b89f904b49f8
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 20:28:07 +0800

    AC (1ms, 88KB)

---
+++ C/d984.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    long long a, b, c;
+    while (scanf(" %lld %lld %lld", &a, &b, &c) == 3)
+    {
+        if (a > b && a > c)
+        {
+            if (a > b + c)puts("A");
+            else if (b > c)puts("B");
+            else puts("C");
+        }
+        else if (b > a && b > c)
+        {
+            if (b > a + c)puts("B");
+            else if (a > c)puts("A");
+            else puts("C");
+        }
+        else
+        {
+            if (c > a + b)puts("C");
+            else if (a > b)puts("A");
+            else puts("B");
+        }
+    }
+    return 0;
+}
