a77b5a8a91c7fd1c57f7606001922aaadc22a96f
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 20:46:00 +0800

    AC (2ms, 108KB)

---
+++ C/d985.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+int main()
+{
+    int track, lap, time, best, sum, min, sec;
+    while (scanf(" %d", &track) == 1)
+    {
+        for (int t = 1; t <= track; t++)
+        {
+            best = 999999, sum = 0;
+            scanf(" %d", &lap);
+            for (int l = 0; l < lap; l++)
+            {
+                scanf(" %d %d", &min, &sec);
+                time = min * 60 + sec;
+                if (time < best)
+                    best = time;
+                sum += time;
+            }
+            sum /= lap;
+            printf("Track %d:\n", t);
+            printf("Best Lap: %d minute(s) %d second(s).\n", best / 60, best % 60);
+            printf("Average: %d minute(s) %d second(s).\n\n", sum / 60, sum % 60);
+        }
+    }
+    return 0;
+}
