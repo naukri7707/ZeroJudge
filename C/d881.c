31cf0571c7f3d4a7a4cc832686a2cc4cf84664b5
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 20:18:17 +0800

    AC (1ms, 72KB)

---
+++ C/d881.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, now, ans, gap;
+    while (scanf(" %d", &n) == 1)
+    {
+        now = gap = 1, ans = 0;
+        for (int i = 0; i < 50; i++)
+            ans += now, now += gap, gap += n;
+        printf("%d\n", ans);
+    }
+    return 0;
+}
