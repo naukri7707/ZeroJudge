e8b996540020a80f47cb52de5d8257ebd3aef414
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-03 17:15:07 +0800

    AC (3ms, 116KB)

---
+++ C/d649.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+char s[1001];
+
+int main()
+{
+    int n;
+    while (scanf(" %d", &n) == 1 && n)
+    {
+        for (int i = 0; i < n; i++)
+            s[i] = '_';
+        s[n] = '\0';
+        for (int i = n - 1; i >= 0; i--)
+        {
+            s[i] = '+';
+            puts(s);
+        }
+    }
+    return 0;
+}
