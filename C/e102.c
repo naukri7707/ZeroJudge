303093917568334bfc00a683337030b77b844481
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-04 17:52:18 +0800

    AC (2ms, 96KB)

---
+++ C/e102.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int kase, n, k;
+    long long ans;
+    scanf(" %d", &kase);
+    while (kase--)
+    {
+        scanf(" %d %d", &n, &k);
+        ans = 1;
+        for (int i = n; i > k; i--)
+            ans *= i;
+        for (int i = n - k; i > 1; i--)
+            ans /= i;
+        printf("%lld\n", ans);
+    }
+    return 0;
+}
