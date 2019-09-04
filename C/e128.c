8b2f2c72c4e1d25854a58ece060448223c84951c
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-04 21:02:02 +0800

    AC (2ms, 148KB)

---
+++ C/e128.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, sr, pos;
+    while (scanf(" %d", &n) == 1 && n)
+    {
+        sr = sqrt((double)n);
+        if (sr * sr == n)
+            sr--;
+        n -= sr * sr;
+        pos = sr + 1;
+        if (pos & 1)
+        {
+            if (n < pos)
+                printf("%d %d\n", pos, n);
+            else
+                printf("%d %d\n", (pos << 1) - n, pos);
+        }
+        else
+        {
+            if (n < pos)
+                printf("%d %d\n", n, pos);
+            else
+                printf("%d %d\n", pos, (pos << 1) - n);
+        }
+    }
+    return 0;
+}
