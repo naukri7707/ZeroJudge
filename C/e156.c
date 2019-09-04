0f3367d443b545fe780bf3fa5ef9ff503531a760
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-04 21:15:10 +0800

    AC (2ms, 108KB)

---
+++ C/e156.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int n, sum;
+
+int dfs()
+{
+    return (sum += n) && --n && dfs();
+}
+
+int main()
+{
+    scanf(" %d", &n);
+    dfs();
+    printf("%d", sum);
+    return 0;
+}
