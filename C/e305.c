abcaf1c01af326bea2295300b9c520e87c8aaf13
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-05 11:02:54 +0800

    AC (6ms, 104KB)

---
+++ C/e305.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+unsigned long long n, ans;
+
+int main()
+{
+    while (scanf(" %llu", &n) == 1)
+    {
+        if (n)
+        {
+            ans = 1;
+            do
+            {
+                if (!(n & 1))
+                    ans <<= 1;
+            } while (n >>= 1);
+            printf("%llu\n", ans);
+        }
+        else puts("0");
+    }
+    return 0;
+}
