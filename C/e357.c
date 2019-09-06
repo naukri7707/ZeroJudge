6a0b5102805a3dc70706e2a009c9de543bfb8115
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-06 21:38:36 +0800

    AC (2ms, 120KB)

---
+++ C/e357.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int F(int x)
+{
+    if (x & 1)
+        return x == 1 ? 1 : F(x - 1) + F(x + 1);
+    else
+        return F(x / 2);
+}
+
+int main()
+{
+    int n;
+    scanf(" %d", &n);
+    printf("%d", F(n));
+    return 0;
+}
