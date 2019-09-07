c726a08a075a66f1baa9f4ce49e5b581f5628c33
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-07 23:29:44 +0800

    AC (2ms, 92KB)

---
+++ C/a863.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n, now, tmp;
+    while (scanf(" %d", &n) == 1)
+    {
+        for (now = n; now != 1 && now != 4; now = tmp)
+        {
+            tmp = 0;
+            while (now)
+            {
+                tmp += (now % 10) * (now % 10);
+                now /= 10;
+            }
+        }
+        if (now == 1)
+            printf("%d is a happy number\n", n);
+        else if (now == 4)
+            printf("%d is an unhappy number\n", n);
+    }
+    return 0;
+}
