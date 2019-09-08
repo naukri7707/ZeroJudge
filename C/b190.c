fafb6003b80ec874a3f6c97f167692b91d16c4dd
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-08 11:23:58 +0800

    AC (7ms, 92KB)

---
+++ C/b190.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int n, count;
+char list[100];
+
+void dfs(int lv, char to)
+{
+    for (int i = lv; i; i--)
+    {
+        if (lv == n)
+            to = i & 1 ? 'b' : 'c';
+        if (list[i] != to)
+        {
+            dfs(i - 1, 294 - list[i] - to);
+            printf("ring %d : %c -> %c\n", i, list[i], to);
+            list[i] = to;
+            count++;
+        }
+    }
+}
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        count = 0;
+        for (int i = 1; i <= n; i++)
+            list[i] = 'a';
+        dfs(n, '?');
+        printf("共移動了 %d 步\n", count);
+    }
+    return 0;
+}
