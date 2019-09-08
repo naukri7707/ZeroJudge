fc9e91e0edebed421a6ebea67ba46a3607b23c96
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-09 00:10:14 +0800

    AC (19ms, 104KB)

---
+++ C/a227.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int n, count;
+char list[100];
+
+void dfs(int lv, char to)
+{
+    for (int i = lv; i; i--)
+    {
+        if (list[i] != to)
+        {
+            dfs(i - 1, 198 - list[i] - to);
+            printf("Move ring %d from %c to %c\n", i, list[i], to);
+            list[i] = to;
+            count++;
+        }
+    }
+}
+
+int main()
+{
+    while (scanf(" %d", &n) == 1)
+    {
+        count = 0;
+        for (int i = 1; i <= n; i++)
+            list[i] = 'A';
+        dfs(n, 'C');
+        putchar('\n');
+    }
+    return 0;
+}
