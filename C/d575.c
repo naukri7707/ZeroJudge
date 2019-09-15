e9a7ec7b863e513a5c5e0cbd818253a8e7f366b3
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-16 01:56:46 +0800

    AC (78ms, 1.1MB)

---
+++ C/d575.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#define BUFSIZ 1048576
+
+inline char readChar()
+{
+    static char buffer[BUFSIZ], * now = buffer + BUFSIZ, * end = buffer + BUFSIZ;
+    if (now == end)
+    {
+        if (end < buffer + BUFSIZ)
+            return EOF;
+        end = (buffer + fread(buffer, 1, BUFSIZ, stdin));
+        now = buffer;
+    }
+    return *now++;
+}
+
+inline char readLongLong(long long* dst)
+{
+    register char ch;
+    while ((ch = readChar()) < '-')
+        if (ch == EOF) return 0;
+    if (ch == '-')
+    {
+        *dst = readChar() ^ '0';
+        while ((ch = readChar()) >= '0')
+            * dst = (*dst << 3) + (*dst << 1) + (ch ^ '0');
+        *dst = ~*dst + 1;
+    }
+    else
+    {
+        *dst = ch ^ '0';
+        while ((ch = readChar()) >= '0')
+            * dst = (*dst << 3) + (*dst << 1) + (ch ^ '0');
+    }
+    return 1;
+}
+
+long long gap(long long num1, long long num2)
+{
+    return num1 > num2 ? num1 - num2 : num2 - num1;
+}
+
+int main()
+{
+    long long dx, dy, cx, cy, da;
+    while (readLongLong(&dx) & readLongLong(&dy) & readLongLong(&cx) & readLongLong(&cy) & readLongLong(&da))
+    {
+        puts(gap(dx, cx) + gap(dy, cy) > da ? "alive" : "die");
+    }
+    return 0;
+}
