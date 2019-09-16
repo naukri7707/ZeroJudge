79a66ef994952b0cab6f522f7a14802b5fe01727
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-09-16 12:07:34 +0800

    AC (2ms, 84KB)

---
+++ C/d250.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int ans;
+    char input[10], output[2] = { 0 };
+    while (gets(input))
+    {
+        ans = 45;
+        for (int i = 0; i < 9; i++)
+            ans -= input[i] ^ '0';
+        output[0] = ans | '0';
+        puts(output);
+    }
+    return 0;
+}
